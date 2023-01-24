f639b14f1808ab176184a31f82ae7b6ea6a80880
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-24 13:58:38 -0700

    added hrefs to the projects github logo

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -7,31 +7,73 @@ function Projects() {
     <ul className="list">
       <li className="newFont project">
         Dev.Log
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/btempini/dev-log"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
       <li className="newFont project">
         DevDive
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/BluSkreen/DevDive"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
       <li className="newFont project">
         National Park Finder
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/HeyItsBradley/National-Park-Finder-with-Directions"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
       <li className="newFont project">
         Agent Select
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/HeyItsBradley/Valorant-Agents"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
       <li className="newFont project">
         Text Editor
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/HeyItsBradley/Text-Editor"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
       <li className="newFont project">
         Weather Dashboard
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/HeyItsBradley/Weather-Dashboard"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
       <li className="newFont project">
         Password Generator
-        <img className="github" src={Github} alt="github icon" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/HeyItsBradley/RandomPasswordGenerator"
+        >
+          <img className="github" src={Github} alt="github icon" />
+        </a>
       </li>
     </ul>
   );
